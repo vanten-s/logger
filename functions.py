3c68ee80dbaef6b4f090f1b74d31895d4bf7c0e8
vanten-s <svante.spannar@gmail.com>
2023-01-12 12:44:25 +0100

    Log Levels

--- functions.py
+++ functions.py
@@ -4,12 +4,30 @@ import datetime
 
 log_files = [ sys.stdout, open( "log.txt", "w" ) ]
 
+def write_to_log_files( string ):
+    now_string = datetime.datetime.now().time().strftime( "%H:%M:%S" )
+    for log_file in log_files:
+        log_file.write( f"[{now_string}] {string}" )
+
+def log_error( string ):
+    write_to_log_files( f"ERROR:   {}" )
+
+def log_info( string ):
+    write_to_log_files( f"INFO:    {}" )
+
+def log_warning ( string ):
+    write_to_log_files( f"WARNING: {}" )
+
 def log( func ):
     def logged_function( *args, **kwargs ):
-        return_val = func( *args, **kwargs )
-        for log_file in log_files:
-            now_string = datetime.datetime.now().time().strftime( "%H:%M:%S" )
-            log_file.write( f"[{now_string}] called {func.__name__}" )
+        try:
+            return_val = func( *args, **kwargs )
+
+        except Exception as e:
+            log_warning( "Function {func.__name__} gave error {e}" )
+            return_val = None
+
+        log_info( "Function {func.__name__} was called" )
 
         return return_val
 
